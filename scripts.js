75c4e75a23f5f445df06f41d31140924b753e929
Bhaskar Kandiyal <bkandiyal@gmail.com>
2017-01-21 00:26:04 +0530

    Added Bootstrap and made the interface more mobile friendly

--- scripts.js
+++ scripts.js
@@ -1,14 +1,40 @@
 $(document).ready(function() {
-  $(".square").mouseenter(function() {
-    $(this).stop(true,true).animate({
-      height: '+=220px'
-    });
-  $(this).find(".content").delay(800).stop(true,true).animate({"opacity":"1"}, 800);
+  $('.expand-square').one('click', expand_mobile);
+
+  $(".square").hover(function() {
+    expand(this);
+  }, function() {
+    restore(this);
   });
-  $(".square").mouseleave(function(){
-    $(this).stop(true,true).animate({
-      height: '-=220px'
+
+  function expand_mobile() {
+    expand($(this).parent());
+    $(this).one('click', restore_mobile);
+  }
+
+  function restore_mobile() {
+    restore($(this).parent());
+    $(this).one('click', expand_mobile);
+  }
+
+  function expand(item) {
+    if ($(item).is(':animated') || $(item).attr('data-oldheight')) return;
+    var oldHeight = $(item).height();
+    var newHeight = $(item).get(0).scrollHeight;
+    $(item).attr('data-oldheight', oldHeight);
+    $(item).stop(true,true).animate({
+      height: newHeight
     });
-  $(".content").delay(100).stop(true,true).animate({"opacity":"0"}, 400);
-  });
+    $(item).find(".content").delay(800).stop(true,true).animate({"opacity":"1"}, 800);
+  }
+
+  function restore(item) {
+    var oldHeight = $(item).attr('data-oldheight');
+    if (!oldHeight) return;
+    $(item).removeAttr('data-oldheight');
+    $(item).stop(true, true).animate({
+      height: oldHeight
+    });
+    $(item).find(".content").delay(100).stop(true,true).animate({"opacity":"0"}, 400);
+  }
 });
